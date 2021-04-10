ee1fbf1a93fe7344a3ae88a726ebf2a71cd4ccdc
kost12-bit <72891196+kost12-bit@users.noreply.github.com>
2021-04-10 11:53:37 +0500

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,67 @@
+document.onkeydown = checkButton;
+
+function checkButton(event)  {
+    let cubeRight = cube.x + cube.width;
+    let cubeBottom = cube.y + cube.height;
+
+    let mapRight = map.x + map.width;
+    let mapBottom = map.y + map.height
+
+        if (event.keyCode === 37) {
+        console.log("ArrowLeft");
+            if (cube.x > 0) {
+            cube.x -= cube.step;
+            }
+        }
+
+        if (event.keyCode === 38) {
+        console.log("ArrowUp");
+            if (cube.y > 0) {
+            cube.y -= cube.step;
+            }
+        }
+
+        if (event.keyCode === 39) {
+        console.log("ArrowRight");
+            if (cubeRight < mapRight) {
+                if ((mapRight - cubeRight) < cube.step){
+                cube.x += (mapRight - cubeRight);
+                } 
+                }
+                else{
+                cube.x += cube.step;
+                }
+            
+        }   
+
+        if (event.keyCode === 40) {
+        console.log("ArrowDown");
+            if (cubeBottom < mapBottom) {
+                if ((mapBottom - cubeBottom) < cube.step){
+                cube.y += (mapBottom - cubeBottom);
+                } 
+                }
+                else{
+                cube.y += cube.step;
+                }
+        }   
+
+    renderCube(cube);
+}
+
+function renderCube(cube) {
+    document.getElementById(cube.id).style.top = cube.y + 'px';
+    document.getElementById(cube.id).style.left = cube.x + 'px';
+    document.getElementById(cube.id).style.width = cube.width + 'px';
+    document.getElementById(cube.id).style.height = cube.height + 'px';
+    document.getElementById(cube.id).style.transition = 'ease ' + cube.trnstn + 's';
+    document.getElementById(cube.id).style.backgroundColor = cube.color;
+}
+
+function renderMap(map) {
+    document.getElementById('map').style.top = map.y + 'px';
+    document.getElementById('map').style.left = map.x + 'px';
+    document.getElementById('map').style.width = map.width + 'px';
+    document.getElementById('map').style.height = map.height + 'px';
+    document.getElementById('map').style.backgroundColor = map.color;
+}
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 let cube = {
     id: 'cube',
     x: 0,
@@ -7,18 +5,18 @@ let cube = {
     width: 56,
     height: 56,
     trnstn: .25,
-    step: 100,
     step: 56,
     color: 'black'
 }
 
 let map = {
-    x: 0,
+    X: 0,
     y: 0,
-    width: 500,
-    height: 500,
-    color: 'F1F1F1',
-    border: 'F2F2F2'
+    width: 504,
+    height: 504,
+    color: 'red',
+    border: '#f1f1f1'
 }
+
 renderMap(map);
 renderCube(cube);
